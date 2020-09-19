ebb704625958cb256c0676eb8bf9fb4254cd4c07
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-09-19 14:56:21 -0400

    Create BuzzyCite automation to Google search and home page.

--- automation.py
+++ automation.py
@@ -1,10 +1,26 @@
 from selenium import webdriver
-import time
+from selenium.webdriver.common.keys import Keys
+from threading import Timer
 
-chrome_browser = webdriver.Chrome('./chromedriver.exe')
 
-chrome_browser.get('https://buzzycite.com')
+def buzzycite_automation():
+	chrome_browser = webdriver.Chrome('./chromedriver.exe')
 
-time.sleep(5)
+	chrome_browser.get('https://buzzycite.com')
 
-chrome_browser.close()
+	chrome_browser.get('https://google.com')
+
+	google_search_input = chrome_browser.find_element_by_class_name('gLFyf')
+	google_search_input.send_keys('buzzycite free court case search')
+	google_search_input.send_keys(Keys.ENTER)
+	buzzycite_link = chrome_browser.find_element_by_xpath("//h3[contains(text(),'BuzzyCite')]")
+
+	buzzycite_link.click()
+
+	chrome_browser.close()
+
+
+def buzzycite_interval():
+    Timer(10000.0, buzzycite_automation).start()
+
+Timer(1.0, buzzycite_automation).start()
