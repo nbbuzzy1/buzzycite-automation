3b338cad784b1ccb18fe959f9079f188b0062a47
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-10-03 07:31:30 -0400

    Separate out buzzycite and google automation; customize sleep time.

--- automation.py
+++ automation.py
@@ -7,21 +7,35 @@ options.add_argument("--headless")
 
 def buzzycite_automation():
 	chrome_browser = webdriver.Chrome('./chromedriver.exe', options=options)
-
 	chrome_browser.get('https://buzzycite.com')
-	time.sleep(61)
-	chrome_browser.get('https://google.com')
+	# time.sleep(30)
+	buzzycite_page_interaction(chrome_browser)
 
-	google_search_input = chrome_browser.find_element_by_class_name('gLFyf')
-	google_search_input.send_keys('buzzycite free court case search')
-	google_search_input.send_keys(Keys.ENTER)
-	buzzycite_url = 'https://www.buzzycite.com/'
-	buzzycite_link = chrome_browser.find_element_by_xpath('//a[@href="'+ buzzycite_url +'"]')
-	time.sleep(5)
-	buzzycite_link.click()
-	time.sleep(61)
+	chrome_browser.get('https://google.com')
+	time.sleep(20)
+	google_page_interaction(chrome_browser)
+	time.sleep(20)
+	buzzycite_page_interaction(chrome_browser)
 	chrome_browser.close()
 	print('successful automation')
+
+
+def buzzycite_page_interaction(chrome_browser):
+	case_search_button = chrome_browser.find_element_by_xpath("//i[text()='search']")
+	case_search_button.click()
+	time.sleep(75)
+	search_panel = chrome_browser.find_element_by_xpath("//mat-panel-title[text()='Case Term or Phrase']")
+	search_panel.click()
+
+
+def google_page_interaction(chrome_browser):
+	google_search_input = chrome_browser.find_element_by_class_name('gLFyf')
+	google_search_input.send_keys('buzzycite court case search')
+	google_search_input.send_keys(Keys.ENTER)
+	time.sleep(35)
+	buzzycite_url = 'https://www.buzzycite.com/'
+	buzzycite_link = chrome_browser.find_element_by_xpath('//a[@href="'+ buzzycite_url +'"]')
+	buzzycite_link.click()
 
 while True:
 	buzzycite_automation()
