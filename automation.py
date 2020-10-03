1d9646b8d40195b8076a41a08d337a6e2f84da04
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-10-03 06:56:07 -0400

    Set timer with automation.

--- automation.py
+++ automation.py
@@ -1,26 +1,28 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from threading import Timer
+import time
 
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")
 
 def buzzycite_automation():
-	chrome_browser = webdriver.Chrome('./chromedriver.exe')
+	chrome_browser = webdriver.Chrome('./chromedriver.exe', options=options)
 
 	chrome_browser.get('https://buzzycite.com')
-
+	time.sleep(61)
 	chrome_browser.get('https://google.com')
 
 	google_search_input = chrome_browser.find_element_by_class_name('gLFyf')
 	google_search_input.send_keys('buzzycite free court case search')
 	google_search_input.send_keys(Keys.ENTER)
-	buzzycite_link = chrome_browser.find_element_by_xpath("//h3[contains(text(),'BuzzyCite')]")
-
+	buzzycite_url = 'https://www.buzzycite.com/'
+	buzzycite_link = chrome_browser.find_element_by_xpath('//a[@href="'+ buzzycite_url +'"]')
+	time.sleep(5)
 	buzzycite_link.click()
-
+	time.sleep(61)
 	chrome_browser.close()
+	print('successful automation')
 
-
-def buzzycite_interval():
-    Timer(10000.0, buzzycite_automation).start()
-
-Timer(1.0, buzzycite_automation).start()
+while True:
+	buzzycite_automation()
+	time.sleep(10800)
